ba7c63070b2d2c5325f48c5ea7e24cb6103e9c56
Mateusz Chodkowski [Ext] <Mateusz.Chodkowski@ext.frontex.europa.eu>
2020-04-16 10:12:41 +0200

    Init

---
+++ precache-manifest.5627fd5e641b71e37bd8408511cb0129.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c31be3e43b1b2a171b824d32163a8b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbba7658175557959cce",
+    "url": "/static/css/main.ba3d9705.chunk.css"
+  },
+  {
+    "revision": "72329643a7c7b1ec1d8a",
+    "url": "/static/js/2.0a72118d.chunk.js"
+  },
+  {
+    "revision": "cbba7658175557959cce",
+    "url": "/static/js/main.b313033b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
